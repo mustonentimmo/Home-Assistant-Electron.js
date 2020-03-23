213af37c7018e63974617beda236e514fd65ac32
mustonentimmo <mustonentimmo@gmail.com>
2020-03-23 16:12:28 +0200

    Apps panel + callback functions

--- Functions/webOS.js
+++ Functions/webOS.js
@@ -1,20 +1,22 @@
 var lgtv = require("lgtv2")({
         url: 'ws://lgwebostv:3000'
 });
-    
 
 
-window.onload = function() {
+
+//Connecting to WebOS
+
+window.onload = function WebOS() {
   
 console.log('Connecting with WebOS')
       
 
     
-  lgtv.on('connect', function () {
+   lgtv.on('connect', function () {
     if (lgtv.on = 'connect'){
   
             console.log("Connected");
-            $('.dot').css('display', 'block');
+            $('.indicator').css('display', 'block');
             $(".deviceName").text('LG TV');
           
     } else {
@@ -24,57 +26,110 @@ console.log('Connecting with WebOS')
     }
         
   });
-    
-//WebOS panel toggle
 
 }
 
-$('#webOS').on('click', function(){
-        $('.btn-container').toggle();   
-        $('.webOSPanel').toggle();
-        $('.back').toggle();
-})
 
-$('.back').on('click', function(){
-        $('.btn-container').toggle();   
-        $('.webOSPanel').toggle();
-        $('.back').toggle();
-})
+//Checking internet connection
+
+console.log('Do i have internet connection: ' + navigator.onLine);
+
+
+window.addEventListener('offline', () => {
+        $('.alert').toggle();
+        $(".alert").text('No Internet Connection!');
+        $('.indicator').toggle();
+        $('.deviceName').toggle();
+});
+
+window.addEventListener('online', () => {
+        $('.alert').toggleClass('alert-danger alert-success')
+        $(".alert").text('Connection restored!');
+        $('.alert-success').delay(5000).fadeOut();
+        $('.indicator').toggle();
+        $('.deviceName').toggle();
+
+});
+
+//----------------------------------WebOS Panel-------------------------------------
+
+//webOS Panel toggle
+        
+$('#webOS').on('click', function(){
+                $('.btn-container').toggle();   
+                $('.webOSPanel').toggle();
+                $('.back').toggle();
+        })
+
+        $('.back').on('click', function(){
+                $('.btn-container').toggle();   
+                $('.webOSPanel').toggle();
+                $('.back').toggle();
+        })
 
 
 //webOS volume control
 
-$('.dial').knob({
-        "min": 0,
-        "max": 100,
-        'width': 150,
-        'height': 150,
-        'displayInput': true,
-        'fgColor': '#70a1ff',
-        'bgColor': '#f5f5f5',
-        'font': 'Montserrat',
-        'fontWeight': '600',
-        'data-angleOffSet': 90,
-        'data-lineCap': 'round',
-        'format' : function (value) {
-                return value + '%';
-             },
+        $('.dial').knob({
+                "min": 0,
+                "max": 100,
+                'width': 150,
+                'height': 150,
+                'displayInput': true,
+                'fgColor': '#70a1ff',
+                'bgColor': '#f5f5f5',
+                'font': 'Montserrat',
+                'fontWeight': '600',
+                'data-angleOffSet': 90,
+                'data-lineCap': 'round',
+                'format' : function (value) {
+                        return value + '%';
+                },
 
 
-        //Change TV volume
-        'release' : function setVolume (v) {
-                console.log('Setting volume to ' + v)
-                lgtv.request('ssap://audio/setVolume', {volume: v});
-       
-        }
+                //Change TV volume
+                'release' : function setVolume (v) {
+                        console.log('Setting volume to ' + v)
+                        lgtv.request('ssap://audio/setVolume', {volume: v});
         
-});
+                }
+                
+        });
+
+//webOS volume off control
+
+
+        $('.volumeOff').on('click', function() {
+                $(this).find('i').toggleClass('fa-volume-off fa-volume-down')
+                if($(this).find('i').hasClass('fa-volume-off')){
+                        lgtv.request('ssap://audio/setMute', {mute: true})
+                } else {
+                        ($(this).find('i').addClass('fa-volume-down'))
+                        lgtv.request('ssap://audio/setMute', {mute: false})
+                }
+                
+        })
 
 
 
+//WebOS Power on/ff
+
+        $('.shutDown').on('click', function() {
+                lgtv.request('ssap://system/turnOff', function (err, res) {
+                        lgtv.disconnect();
+                });
+        })
 
 
+//WebOS Apps
 
+        $('.netflixBtn').on('click', function() {
+                lgtv.request('ssap://system.launcher/launch', {id: 'netflix'});
+                console.log('Opening Netflix')
+        });
+        
 
-
-   
+        $('.youtubeBtn').on('click', function() {
+                lgtv.request('ssap://system.launcher/launch', {id: 'youtube.leanback.v4'});
+                console.log('Opening Youtube')
+        });
